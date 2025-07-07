07e8bb7467c33031921d691a8ae12a07609ca1f1
ankushgarg <ankushgarg7123@gmail.com>
2025-07-07 08:32:09 -0700

    Changed table name to matches through flyway

--- src/main/resources/db/migration/V5__name_change_match.sql
+++ src/main/resources/db/migration/V5__name_change_match.sql
@@ -1,8 +1,8 @@
 ALTER TABLE odds
 DROP
-FOREIGN KEY FK_ODDS_ON_MATCH;
+FOREIGN KEY FKii9s1cemay7tygltdu3kpsao9;
 
-CREATE TABLE matches
+CREATE TABLE `match`
 (
     id            BIGINT AUTO_INCREMENT NOT NULL,
     created_at    datetime NULL,
@@ -12,8 +12,17 @@ CREATE TABLE matches
     start_time    datetime     NOT NULL,
     score_summary VARCHAR(255) NULL,
     status        VARCHAR(255) NULL,
-    CONSTRAINT pk_matches PRIMARY KEY (id)
+    CONSTRAINT pk_match PRIMARY KEY (id)
 );
 
 ALTER TABLE odds
-    ADD CONSTRAINT FK_ODDS_ON_MATCH FOREIGN KEY (match_id) REFERENCES matches (id);
+    ADD CONSTRAINT FK_ODDS_ON_MATCH FOREIGN KEY (match_id) REFERENCES `match` (id);
+
+DROP TABLE matches;
+
+ALTER TABLE odds
+DROP
+COLUMN market_type;
+
+ALTER TABLE odds
+    ADD market_type VARCHAR(255) NULL;
